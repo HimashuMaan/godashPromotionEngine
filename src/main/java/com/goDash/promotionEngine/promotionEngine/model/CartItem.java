d618bc537243c8dd09f1cc6f3a4662365bdba4a9
himanshu <himanshu.rajroxx@gmail.com>
2025-07-05 15:03:57 +0530

    Added models

--- src/main/java/com/goDash/promotionEngine/promotionEngine/model/CartItem.java
+++ src/main/java/com/goDash/promotionEngine/promotionEngine/model/CartItem.java
@@ -1,4 +1,11 @@
 package com.goDash.promotionEngine.promotionEngine.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class CartItem {
+    private SKU sku;
+    private int quantity;
 }
--- src/main/java/com/goDash/promotionEngine/promotionEngine/model/SKU.java
+++ src/main/java/com/goDash/promotionEngine/promotionEngine/model/SKU.java
@@ -1,4 +1,11 @@
 package com.goDash.promotionEngine.promotionEngine.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SKU {
+    String id;
+    float price;
 }
