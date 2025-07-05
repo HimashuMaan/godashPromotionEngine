b6b96c842b95c978ac30ed53009ce9a00e556547
himanshu <himanshu.rajroxx@gmail.com>
2025-07-05 15:11:29 +0530

    Added cart item and cart request Dto

--- src/main/java/com/goDash/promotionEngine/promotionEngine/dto/CartItemDto.java
+++ src/main/java/com/goDash/promotionEngine/promotionEngine/dto/CartItemDto.java
@@ -1,4 +1,11 @@
 package com.goDash.promotionEngine.promotionEngine.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CartItemDto {
+    private String skuId;
+    private int quantity;
 }
--- src/main/java/com/goDash/promotionEngine/promotionEngine/dto/CartRequest.java
+++ src/main/java/com/goDash/promotionEngine/promotionEngine/dto/CartRequest.java
@@ -1,4 +1,12 @@
 package com.goDash.promotionEngine.promotionEngine.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class CartRequest {
+    private List<CartItemDto> items;
 }
